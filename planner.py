c14a1c9895938bc685e366b655c35edab7eb26b9
okanergknil <72084461+okanergknil@users.noreply.github.com>
2026-04-16 14:59:13 +0300

    Update planner.py

--- planner.py
+++ planner.py
@@ -1,1 +1,6 @@
+# Kendi değerlerini buraya gir (Mevcut İrtifa, Hedef İrtifa, Hız, Kalan Mesafe)
+tod, vsi, durum = descent_calculator(7500, 1500, 110, 25)
 
+print(f"Alçalmaya Başlama Mesafesi (TOD): {tod} NM")
+print(f"Gereken Alçalma Varyosu: {vsi} fpm")
+print(f"Mevcut Durum: {durum}")
